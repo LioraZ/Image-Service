474dfec640944cb34a067afc33ddebbca22b8750
LioraZ <liorazr@gmail.com>
2018-04-04 13:08:26 +0300

    Initial commit

---
+++ ImageWindowsService/ImageWindowsService/ImageService.Logging/Model/MessageRecievedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Logging.Model
+{
+    public class MessageRecievedEventArgs : EventArgs
+    {
+        public MessageRecievedEventArgs(string message, MessageTypeEnum type)
+        {
+            Status = type;
+            Message = message;
+        }
+        public MessageTypeEnum Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ImageWindowsService/ImageWindowsService/ImageService/Model/ImageServiceModel.cs
@@ -0,0 +1,77 @@
+﻿using ImageService.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageService.Model
+{
+    public class ImageServiceModel : IImageServiceModel
+    {
+        #region Members
+        private string m_OutputFolder;            // The Output Folder
+        private int m_thumbnailSize;              // The Size Of The Thumbnail Size
+        #endregion
+
+        public string AddFile(string path, out bool result)
+        {
+            return MoveFile(path, m_OutputFolder, out result);
+        }
+        public string CreateFolder(string path, out bool result)
+        {
+           PathExists(path, out result);
+           if (!result)
+           {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                    result = true;
+                    return "New folder created successfully!";
+                }
+                catch (IOException e)
+                {
+                    result = false;
+                    return "An exception ocuured./n Please try again";
+                }
+                
+           }
+            result = true;
+            return "Folder already exists!";
+
+        }
+        public string MoveFile(string src, string dst, out bool result)
+        {
+            string errorMsg = OutputFolderExists(dst, out result);
+            if (!result) { return errorMsg; }
+            errorMsg = PathExists(src, out result);
+            if (!result) { return errorMsg; }
+            //check edge cases
+            try
+            {
+                File.Copy(src, m_OutputFolder);
+                result = true;
+                return "File successfully added!";
+            }
+            catch (IOException e)
+            {
+                result = false;
+                return "File exists at output folder!";
+
+            }
+        }
+        private string OutputFolderExists(string dir, out bool result) {
+            result = Directory.Exists(dir);
+            return "Output folder not found at path!";
+        }
+        private string PathExists(string path, out bool result)
+        {
+            result = File.Exists(path);
+            return "Image not found at path!";
+        }
+    }
+}
